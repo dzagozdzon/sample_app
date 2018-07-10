eb56e93b825da0c69575ba47108b05704893e59e
dzagozdzon <zagozdzond@gmail.com>
2018-07-11 01:23:56 +0200

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,18 @@
 module SessionsHelper
 
+  # Returns the current logged-in user (if any).
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+  
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
@@ -16,26 +29,20 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
-
-  # Returns the user corresponding to the remember token cookie.
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
+  
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
 
-  # Forgets a persistent session.
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+    # Forgets a persistent session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -48,8 +55,8 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-
-  # Redirects to stored location (or to the default).
+  
+    # Redirects to stored location (or to the default).
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url] || default)
     session.delete(:forwarding_url)
